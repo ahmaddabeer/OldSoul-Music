9e1a5c03bf53972ea40e5e4a4a90eca28407b9ae
Your Name <ahmaddabeer36@gmail.com>
2024-12-05 02:37:50 +0530

    html and css almost complite but work in progress

---
+++ Script.js
@@ -0,0 +1,6 @@
+console.log("Welcome to Old Soul Music ");
+let songs = [
+    { songName: "Bazigar O Bazigar, filePath: "./ song / Bazigar.mp3, coverPath: " covers/" }
+]
+let audioElement = new Audio('Bazigar.mp3');
+// audioElement.play()
